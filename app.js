fba37c367fea5d4291e24c38d761d482d687ba63
Ian Lee <ian102423@github.com>
2020-06-09 17:18:33 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -45,67 +45,52 @@ app.use(session({ // configuration
   store: new FileStore() // to user hard-disk
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) { // basic authentication
   console.log(req.session);
 
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) { // if not
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err); // send error message back to client
-    }
+  function auth(req, res, next) {
+    console.log(req.session);
 
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); // Buffer - we don't need to use it or not require it. from - decode username && password
-    const user = auth[0];
-    const pass = auth[1];
-
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin';
-      return next(); // authorized
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  } else { // if theres sign cookie from incoming value, check if the cookie is the admin
-    if (req.session.user === 'admin') {
-      console.log('req.session:', req.session);
-      return next();
-    } else {
+    if (!req.session.user) {
       const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);
+    } else {
+      if (req.session.user === 'authenticated') {
+        return next();
+      } else {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+      }
     }
   }
-}
 
-app.use(auth);
+  app.use(auth);
 
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+  app.use('/campsites', campsiteRouter);
+  app.use('/promotions', promotionRouter);
+  app.use('/partners', partnerRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-module.exports = app;
+  module.exports = app;
