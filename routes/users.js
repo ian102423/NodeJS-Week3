665570ff9edabb34325107dd619584d68c0f41ff
Ian Lee <ian102423@github.com>
2020-06-17 19:21:08 -0500

    CORS

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
+const cors = require('./cors'); // ./ is important
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find()
     .then(users => {
       res.statusCode = 200;
@@ -16,7 +17,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, ne
     .catch(err => next(err));
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
   User.register(new User({ username: req.body.username }),
     req.body.password, (err, user) => {
       if (err) {
@@ -47,14 +48,14 @@ router.post('/signup', (req, res) => {
     });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => { // login end point
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => { // login end point
   const token = authenticate.getToken({ _id: req.user._id }); // give token to user
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
-router.get('/logout', (req, res, next) => { // logout
+router.get('/logout', cors.corsWithOptions, (req, res, next) => { // logout
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
